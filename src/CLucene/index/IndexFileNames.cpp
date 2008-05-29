17264e0b94db760cdeab03945cd644191415a56a
jweir77 <jweir77@20ef185c-fe11-0410-a618-ba9304b01011>
2008-05-29 20:34:16 +0000

    New IndexFileNames constants for 2.3.2
    git-svn-id: https://clucene.svn.sourceforge.net/svnroot/clucene/branches/lucene2_3_2@2640 20ef185c-fe11-0410-a618-ba9304b01011

--- src/CLucene/index/IndexFileNames.cpp
+++ src/CLucene/index/IndexFileNames.cpp
@@ -10,27 +10,86 @@ CL_NS_DEF(index)
 	const char* IndexFileNames::SEGMENTS_GEN = "segments.gen";
 	const char* IndexFileNames::DELETABLE = "deletable";
 	const char* IndexFileNames::NORMS_EXTENSION = "nrm";
+	const char* IndexFileNames::FREQ_EXTENSION = "frq";
+	const char* IndexFileNames::PROX_EXTENSION = "prx";
+	const char* IndexFileNames::TERMS_EXTENSION = "tis";
+	const char* IndexFileNames::TERMS_INDEX_EXTENSION = "tii";
+	const char* IndexFileNames::FIELDS_INDEX_EXTENSION = "fdx";
+	const char* IndexFileNames::FIELDS_EXTENSION = "fdt";
+	const char* IndexFileNames::VECTORS_FIELDS_EXTENSION = "tvf";
+	const char* IndexFileNames::VECTORS_DOCUMENTS_EXTENSION = "tvd";
+	const char* IndexFileNames::VECTORS_INDEX_EXTENSION = "tvx";
 	const char* IndexFileNames::COMPOUND_FILE_EXTENSION = "cfs";
+	const char* IndexFileNames::COMPOUND_FILE_STORE_EXTENSION = "cfx";
 	const char* IndexFileNames::DELETES_EXTENSION = "del";
+	const char* IndexFileNames::FIELD_INFOS_EXTENSION = "fnm";
 	const char* IndexFileNames::PLAIN_NORMS_EXTENSION = "f";
 	const char* IndexFileNames::SEPARATE_NORMS_EXTENSION = "s";
+	const char* IndexFileNames::GEN_EXTENSION = "gen";
 
 	const char* IndexFileNames::INDEX_EXTENSIONS[] = {
-			"cfs", "fnm", "fdx", "fdt", "tii", "tis", "frq", 
-			"prx", "del", "tvx", "tvd", "gen", "nrm"
+		COMPOUND_FILE_EXTENSION,
+		FIELD_INFOS_EXTENSION,
+		FIELDS_INDEX_EXTENSION,
+		FIELDS_EXTENSION,
+		TERMS_INDEX_EXTENSION,
+		TERMS_EXTENSION,
+		FREQ_EXTENSION,
+		PROX_EXTENSION,
+		DELETES_EXTENSION,
+		VECTORS_INDEX_EXTENSION,
+		VECTORS_DOCUMENTS_EXTENSION,
+		VECTORS_FIELDS_EXTENSION,
+		GEN_EXTENSION,
+		NORMS_EXTENSION,
+		COMPOUND_FILE_STORE_EXTENSION
 	};
 
 	const char* IndexFileNames::INDEX_EXTENSIONS_IN_COMPOUND_FILE[] = {
-			"fnm", "fdx", "fdt", "tii", "tis", "frq", "prx",
-			"tvx", "tvd", "tvf", "nrm"
+		FIELD_INFOS_EXTENSION,
+		FIELDS_INDEX_EXTENSION,
+		FIELDS_EXTENSION,
+		TERMS_INDEX_EXTENSION,
+		TERMS_EXTENSION,
+		FREQ_EXTENSION,
+		PROX_EXTENSION,
+		VECTORS_INDEX_EXTENSION,
+		VECTORS_DOCUMENTS_EXTENSION,
+		VECTORS_FIELDS_EXTENSION,
+		NORMS_EXTENSION
+	};
+	
+	const char* IndexFileNames::STORE_INDEX_EXTENSIONS[] = {
+		VECTORS_INDEX_EXTENSION,
+		VECTORS_FIELDS_EXTENSION,
+		VECTORS_DOCUMENTS_EXTENSION,
+		FIELDS_INDEX_EXTENSION,
+		FIELDS_EXTENSION
+	};
+	
+	const char* IndexFileNames::NON_STORE_INDEX_EXTENSIONS[] = {
+		FIELD_INFOS_EXTENSION,
+		FREQ_EXTENSION,
+		PROX_EXTENSION,
+		TERMS_EXTENSION,
+		TERMS_INDEX_EXTENSION,
+		NORMS_EXTENSION
 	};
 	
 	const char* IndexFileNames::COMPOUND_EXTENSIONS[] = {
-			"fnm", "frq", "prx", "fdx", "fdt", "tii", "tis"
+		FIELD_INFOS_EXTENSION,
+		FREQ_EXTENSION,
+		PROX_EXTENSION,
+		FIELDS_INDEX_EXTENSION,
+		FIELDS_EXTENSION,
+		TERMS_INDEX_EXTENSION,
+		TERMS_EXTENSION
 	};
 	
 	const char* IndexFileNames::VECTOR_EXTENSIONS[] = {
-			"tvx", "tvd", "tvf"
+		VECTORS_INDEX_EXTENSION,
+		VECTORS_DOCUMENTS_EXTENSION,
+		VECTORS_FIELDS_EXTENSION
 	};
 	
 	const char* IndexFileNames::fileNameFromGeneration( const char* base, const char* extension, int64_t gen ) {
@@ -50,5 +109,5 @@ CL_NS_DEF(index)
 
 		return STRDUP_AtoA( fileName );
 	}
-	
+		
 CL_NS_END
