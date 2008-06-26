22f9d40320e3deeaa8d6aaa7a770077c20a21dae
ustramooner <ustramooner@20ef185c-fe11-0410-a618-ba9304b01011>
2008-06-26 16:36:15 +0000

    git-svn-id: https://clucene.svn.sourceforge.net/svnroot/clucene/branches/lucene2_3_2@2672 20ef185c-fe11-0410-a618-ba9304b01011

--- src/CLucene/clucene-config.h.cmake
+++ src/CLucene/clucene-config.h.cmake
@@ -5,350 +5,254 @@
 *  Generated automatically at end of cmake.
 */
 
-/* Compulsary headers. cmake will fail if these are not found:
- * Eventually we will take these out of StdHeader to simplify it all a bit.
-*/
-#define _CL_HAVE_ALGORITHM 1
-#define _CL_HAVE_FUNCTIONAL  1
-#define _CL_HAVE_MAP  1
-#define _CL_HAVE_VECTOR  1
-#define _CL_HAVE_LIST  1
-#define _CL_HAVE_SET  1
-#define _CL_HAVE_MATH_H  1 
-#define _CL_HAVE_STDARG_H  1
-#define _CL_HAVE_STDEXCEPT 1
-#define _CL_HAVE_FCNTL_H  1 
-
-#define _CL_HAVE_WCSCPY  1 
-#define _CL_HAVE_WCSNCPY  1 
-#define _CL_HAVE_WCSCAT  1 
-#define _CL_HAVE_WCSCHR  1 
-#define _CL_HAVE_WCSSTR  1 
-#define _CL_HAVE_WCSLEN  1 
-#define _CL_HAVE_WCSCMP  1 
-#define _CL_HAVE_WCSNCMP  1 
-#define _CL_HAVE_WCSCSPN  1 
-
 /* CMake will look for these headers: */
-
-/* Define to 1 if you have the ANSI C header files. */
-#cmakedefine _CL_STDC_HEADERS  1 
-
-/* Define to 1 if you have the <string.h> header file. */
 #cmakedefine _CL_HAVE_STRING_H  1
-
-/* Define to 1 if you have the <strings.h> header file. */
-#cmakedefine _CL_HAVE_STRINGS_H  1 
-
-/* Define to 1 if you have the <memory.h> header file. */
 #cmakedefine _CL_HAVE_MEMORY_H  1 
-
-/* Define to 1 if you have the <unistd.h> header file. */
 #cmakedefine _CL_HAVE_UNISTD_H  1 
-
-/* Define to 1 if you have the <io.h> header file. */
 #cmakedefine _CL_HAVE_IO_H  1 
-
-/* Define to 1 if you have the <direct.h> header file. */
 #cmakedefine _CL_HAVE_DIRECT_H  1 
-
-/* Define to 1 if you have the <dirent.h> header file, and it defines `DIR'. */
 #cmakedefine _CL_HAVE_DIRENT_H  1 
-
-/* Define to 1 if you have the <sys/dir.h> header file, and it defines `DIR'. */
 #cmakedefine _CL_HAVE_SYS_DIR_H
-
-/* Define to 1 if you have the <sys/ndir.h> header file, and it defines `DIR'. */
 #cmakedefine _CL_HAVE_SYS_NDIR_H
-
-/* Define to 1 if you have the <errno.h> header file. */
 #cmakedefine _CL_HAVE_ERRNO_H  1 
-
-/* Define to 1 if you have the <wchar.h> header file. */
 #cmakedefine _CL_HAVE_WCHAR_H  1 
-
-/* Define to 1 if you have a functioning <wchar.h> header file. */
 #cmakedefine _CL_HAVE_WCTYPE_H   
-
-/* Define to 1 if you have the <ctype.h> header file. */
 #cmakedefine _CL_HAVE_CTYPE_H  1 
-
-/* Define to 1 if you have the <windows.h> header file. */
 #cmakedefine _CL_HAVE_WINDOWS_H  1 
-
-/* Define to 1 if you have the <sys/types.h> header file. */
 #cmakedefine _CL_HAVE_SYS_TYPES_H  1 
-
-/* Define to 1 if you have the <inttypes.h> header file. */
-#cmakedefine _CL_HAVE_INTTYPES_H  1 
-
-
-/* CMake will look for these symbols: */
-
-/* Define to 1 if the system has the type `float_t'. */
-//todo: HACK
-#define _CL_HAVE_FLOAT_T  1 
-
-#ifdef __MINGW32__
-typedef float float_t;
-#endif
-
-
-/* Define to 1 if the system has the type `intptr_t'. */
-//todo: HACK
-#define _CL_HAVE_INTPTR_T  1 
-
-/* Define to 1 if the system has the type `wchar_t'. */
-//todo: HACK
-#define _CL_HAVE_WCHAR_T  1 
-
+#cmakedefine _CL_HAVE_DLFCN_H  1 
+#cmakedefine _CL_HAVE_FCNTL_H  1 
+#cmakedefine _CL_HAVE_EXT_HASH_MAP  1 
+#cmakedefine _CL_HAVE_EXT_HASH_SET  1 
+#cmakedefine _CL_HAVE_HASH_MAP
+#cmakedefine _CL_HAVE_HASH_SET
+#cmakedefine _CL_HAVE_NDIR_H
+#cmakedefine _CL_HAVE_SYS_STAT_H  1 
+#cmakedefine _CL_HAVE_SYS_TIMEB_H  1 
+#cmakedefine _CL_HAVE_SYS_TIME_H 1
+#cmakedefine _CL_HAVE_TCHAR_H 1
 
 
 /* CMake will look for these functions: */
+#cmakedefine _CL_HAVE__VSNWPRINTF
+#cmakedefine _CL_HAVE__SNWPRINTF
+#cmakedefine _CL_HAVE_WCSCASECMP
+#cmakedefine _CL_HAVE_WCSCAT  1 
+#cmakedefine _CL_HAVE_WCSCHR  1 
+#cmakedefine _CL_HAVE_WCSCMP  1 
+#cmakedefine _CL_HAVE_WCSCPY  1 
+#cmakedefine _CL_HAVE_WCSCSPN  1 
+#cmakedefine _CL_HAVE_WCSICMP
+#cmakedefine _CL_HAVE_WCSLEN  1 
+#cmakedefine _CL_HAVE_WCSNCMP  1 
+#cmakedefine _CL_HAVE_WCSNCPY  1 
+#cmakedefine _CL_HAVE_WCSSTR  1 
+#cmakedefine _CL_HAVE_WCSTOD
+#cmakedefine _CL_HAVE_WCSTOLL
+#cmakedefine _CL_HAVE_WCSUPR
+#cmakedefine _CL_HAVE_GETTIMEOFDAY
 
-/* Define to 1 if you have the `printf' function. */
+#cmakedefine _CL_HAVE_LLTOA
+#cmakedefine _CL_HAVE_LLTOW
 #cmakedefine _CL_HAVE_PRINTF  1 
-
-/* Define to 1 if you have the `snprintf' function. */
 #cmakedefine _CL_HAVE_SNPRINTF  1 
- 
+#cmakedefine _CL_HAVE_MMAP  1 
+#cmakedefine _CL_HAVE_NANOSLEEP  1 
+#cmakedefine _CL_HAVE_STRLWR
+#cmakedefine _CL_HAVE_STRTOLL
+#cmakedefine _CL_HAVE_STRUPR
+#cmakedefine _CL_HAVE_GETPAGESIZE
+
+#define CL_NS_HASHING(func) ${CL_NS_HASHING_VALUE}
+
+${SYMBOL_CL_MAX_PATH}
+${SYMBOL_O_RANDOM}
+${SYMBOL_O_BINARY}
+${SYMBOL__S_IREAD}
+${SYMBOL__S_IWRITE}
+
+#define _ILONG(x) x ## L
+#define _ILONGLONG(x) ${_CL_ILONGLONG_VALUE}
+
+${FUNCTION_FILESTAT}
+${FUNCTION_FILESIZE}
+${FUNCTION_FILESEEK}
+${FUNCTION_FILETELL}
+${FUNCTION_FILEHANDLESTAT}
+${FUNCTION__REALPATH}
+${FUNCTION__RENAME}
+${FUNCTION__CLOSE}
+${FUNCTION__READ}
+${FUNCTION__OPEN}
+${FUNCTION__WRITE}
+${FUNCTION__SNPRINTF}
+${FUNCTION__MKDIR}
+${FUNCTION__UNLINK}
 
 
-/* CMake will determine these module specificics */
+// our needed types
+#if !@HAVE_INT8_T@
+ #define HAVE_INT8_T 1
+ #if ${SIZEOF_CHAR}==1 //is char one byte?
+  typedef signed char int8_t;
+ #else
+  #error Could not determine type for int8_t!
+ #endif
+#endif
+
+#if !@HAVE_UINT8_T@
+ #define HAVE_UINT8_T 1
+ #if ${SIZEOF_CHAR}==1 //is char one byte?
+  typedef unsigned char uint8_t;
+ #else
+  #error Could not determine type for uint8_t!
+ #endif
+#endif
+
+#if !@HAVE_INT16_T@
+ #define HAVE_INT16_T 1
+ #if ${SIZEOF_SHORT}==2 //is short two bytes?
+  typedef signed short int16_t;
+ #else
+  #error Could not determine type for int16_t!
+ #endif
+#endif
+
+#if !@HAVE_UINT16_T@
+ #define HAVE_UINT16_T 1
+ #if ${SIZEOF_SHORT}==2 //is short two bytes?
+  typedef unsigned short uint16_t;
+ #else
+  #error Could not determine type for uint16_t!
+ #endif
+#endif
+
+#if !@HAVE_INT32_T@
+ #define HAVE_INT32_T 1
+ #if ${SIZEOF_INT}==4 //is int four bytes?
+  typedef signed int int32_t;
+ #elif ${SIZEOF_LONG}==4 //is long four bytes?
+  typedef signed long int32_t;
+ #else
+  #error Could not determine type for int32_t!
+ #endif
+#endif
+
+#if !@HAVE_UINT32_T@
+ #define HAVE_UINT32_T 1
+ #if ${SIZEOF_INT}==4 //is int four bytes?
+  typedef unsigned int uint32_t;
+ #elif ${SIZEOF_LONG}==4 //is long four bytes?
+  typedef unsigned long uint32_t;
+ #else
+  #error Could not determine type for uint32_t!
+ #endif
+#endif
+
+#if !@HAVE_INT64_T@
+    #define HAVE_INT64_T 1
+    typedef signed ${TYPEOF_INT64} int64_t;
+#endif
+
+#if !@HAVE_UINT64_T@
+    #define HAVE_UINT64_T 1
+    typedef unsigned ${TYPEOF_INT64} uint64_t;
+#endif
+
+#define _LUCENE_PRAGMA_WARNINGS
+
+//todo: not sure if this detects correctly? check float.h?
+#if !@HAVE_FLOAT_T@
+    typedef double float_t;
+#endif
+
+//todo not sure if this detects correctly... (should check string.h??)
+#if !@HAVE_SIZE_T@
+ #ifndef _SIZE_T_DEFINED 
+  #ifndef HAVE_SIZE_T
+   typedef unsigned int size_t;
+   #define HAVE_SIZE_T 1
+  #endif
+  #define _SIZE_T_DEFINED 1     // kdewin32 define
+ #endif
+#endif
+
+
+/* CMake will determine these specifics. Things like bugs, etc */
+
+/* if we can't support the map/set hashing */
+#cmakedefine LUCENE_DISABLE_HASHING 1
 
 /* Define if you have POSIX threads libraries and header files. */
 #cmakedefine _CL_HAVE_PTHREAD  1 
 
-/* Define if you have POSIX threads libraries and header files. */
+/* Define if you have Win32 threads libraries and header files. */
 #cmakedefine _CL_HAVE_WIN32_THREADS  1 
 
-/* define if the compiler supports ISO C++ standard library */
-#cmakedefine _CL_HAVE_STD   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* Disable multithreading */
-#cmakedefine _CL_DISABLE_MULTITHREADING
-
-
-/* Define to 1 if you have the <dlfcn.h> header file. */
-#cmakedefine _CL_HAVE_DLFCN_H  1 
-
-
-/* Define to 1 if you have the <ext/hash_map> header file. */
-#cmakedefine _CL_HAVE_EXT_HASH_MAP  1 
-
-/* Define to 1 if you have the <ext/hash_set> header file. */
-#cmakedefine _CL_HAVE_EXT_HASH_SET  1 
-
-
-/* Define to 1 if you have the <functional> header file. */
-
-/* Does not support new float byte<->float conversions */
-#cmakedefine _CL_HAVE_FUNCTIONING_FLOAT_BYTE   
-
-/* Define to 1 if you have the `getpagesize' function. */
-#cmakedefine _CL_HAVE_GETPAGESIZE
-
-/* Define to 1 if you have the <hash_map> header file. */
-#cmakedefine _CL_HAVE_HASH_MAP
-
-/* Define to 1 if you have the <hash_set> header file. */
-#cmakedefine _CL_HAVE_HASH_SET
-
-
-/* Define to 1 if you have the `lltoa' function. */
-#cmakedefine _CL_HAVE_LLTOA
-
-/* Define to 1 if you have the `lltow' function. */
-#cmakedefine _CL_HAVE_LLTOW
-
-/* Define to 1 if long double works and has more range or precision than double. */
-#cmakedefine _CL_HAVE_LONG_DOUBLE  1 
-
-
-
-
-/* Define to 1 if you have a working `mmap' system call. */
-#cmakedefine _CL_HAVE_MMAP  1 
+/* Define what eval method is required for float_t to be defined (for GCC). */
+#cmakedefine _FLT_EVAL_METHOD  ${_FLT_EVAL_METHOD} 
 
 /* define if the compiler implements namespaces */
 #cmakedefine _CL_HAVE_NAMESPACES   
 
-/* Define if you have the nanosleep function */
-#cmakedefine _CL_HAVE_NANOSLEEP  1 
-
-/* Define to 1 if you have the <ndir.h> header file, and it defines `DIR'. */
-#cmakedefine _CL_HAVE_NDIR_H
-
 /* Does not support new float byte<->float conversions */
+//todo: not being checked for...
 #cmakedefine _CL_HAVE_NO_FLOAT_BYTE
 
 /* Does not support try/catch blocks */
+//todo: not checked properly
 #cmakedefine _CL_HAVE_NO_FUNCTION_TRY_BLOCKS
 
-
 /* Define if recursive pthread mutexes are available */
+//todo: not being checked for...
 #cmakedefine _CL_HAVE_PTHREAD_MUTEX_RECURSIVE  1 
-
-
 
 /* Defined if the snprintf overflow test fails */
 #cmakedefine _CL_HAVE_SNPRINTF_BUG
 
-/* Define to 1 if you have the `snwprintf' function. */
-#cmakedefine _CL_HAVE_SNWPRINTF
-
-
-
-/* Define to 1 if you have the <stdint.h> header file. */
-#cmakedefine _CL_HAVE_STDINT_H  1 
-
-/* Define to 1 if you have the <stdlib.h> header file. */
-#cmakedefine _CL_HAVE_STDLIB_H  1 
-
-/* define if the compiler supports Standard Template Library */
-#cmakedefine _CL_HAVE_STL   
-
-
-/* Define to 1 if you have the `strlwr' function. */
-#cmakedefine _CL_HAVE_STRLWR
-
-/* Define to 1 if you have the `strtoll' function. */
-#cmakedefine _CL_HAVE_STRTOLL
-
-/* Define to 1 if you have the `strupr' function. */
-#cmakedefine _CL_HAVE_STRUPR
-
 /* Defined if the swprintf test fails */
-#cmakedefine _CL_HAVE_SWPRINTF_BUG   
-
-/* Define to 1 if you have the <sys/stat.h> header file. */
-#cmakedefine _CL_HAVE_SYS_STAT_H  1 
-
-/* Define to 1 if you have the <sys/timeb.h> header file. */
-#cmakedefine _CL_HAVE_SYS_TIMEB_H  1 
-
-
-/* Define to 1 if you have the <tchar.h> header file. */
-#cmakedefine _CL_HAVE_TCHAR_H
-
-/* Define to 1 if you have the `tell' function. */
-#cmakedefine _CL_HAVE_TELL
-
-
-
-/* Define to 1 if you have the `vsnwprintf' function. */
-#cmakedefine _CL_HAVE_VSNWPRINTF
-
-/* Define to 1 if you have the `wcscasecmp' function. */
-#cmakedefine _CL_HAVE_WCSCASECMP
-
-/* Define to 1 if you have the `wcscat' function. */
-#cmakedefine _CL_HAVE_WCSCAT  1 
-
-/* Define to 1 if you have the `wcschr' function. */
-#cmakedefine _CL_HAVE_WCSCHR  1 
-
-/* Define to 1 if you have the `wcscmp' function. */
-#cmakedefine _CL_HAVE_WCSCMP  1 
-
-/* Define to 1 if you have the `wcscpy' function. */
-#cmakedefine _CL_HAVE_WCSCPY  1 
-
-/* Define to 1 if you have the `wcscspn' function. */
-#cmakedefine _CL_HAVE_WCSCSPN  1 
-
-/* Define to 1 if you have the `wcsicmp' function. */
-#cmakedefine _CL_HAVE_WCSICMP
-
-/* Define to 1 if you have the `wcslen' function. */
-#cmakedefine _CL_HAVE_WCSLEN  1 
-
-/* Define to 1 if you have the `wcsncmp' function. */
-#cmakedefine _CL_HAVE_WCSNCMP  1 
-
-/* Define to 1 if you have the `wcsncpy' function. */
-#cmakedefine _CL_HAVE_WCSNCPY  1 
-
-/* Define to 1 if you have the `wcsstr' function. */
-#cmakedefine _CL_HAVE_WCSSTR  1 
-
-/* Define to 1 if you have the `wcstod' function. */
-#cmakedefine _CL_HAVE_WCSTOD
-
-/* Define to 1 if you have the `wcstoll' function. */
-#cmakedefine _CL_HAVE_WCSTOLL
-
-/* Define to 1 if you have the `wcsupr' function. */
-#cmakedefine _CL_HAVE_WCSUPR
-
-
-/* Define to 1 if you have the `wprintf' function. */
-#cmakedefine _CL_HAVE_WPRINTF
-
-/* Define to 1 if you have the `_filelength' function. */
-#cmakedefine _CL_HAVE__FILELENGTH
+#cmakedefine _CL_HAVE_SNWPRINTF_BUG   
 
 /* How to define a static const in a class */
-#cmakedefine LUCENE_STATIC_CONSTANT_SYNTAX  1 
-
-/* Name of package */
-#cmakedefine _CL_PACKAGE  "clucene-core" 
-
-/* Define to the address where bug reports for this package should be sent. */
-#cmakedefine _CL_PACKAGE_BUGREPORT  "" 
-
-/* Define to the full name of this package. */
-#cmakedefine _CL_PACKAGE_NAME  "" 
-
-/* Define to the full name and version of this package. */
-#cmakedefine _CL_PACKAGE_STRING  "" 
-
-/* Define to the one symbol short name of this package. */
-#cmakedefine _CL_PACKAGE_TARNAME  "" 
-
-/* Define to the version of this package. */
-#cmakedefine _CL_PACKAGE_VERSION  "" 
+#define LUCENE_STATIC_CONSTANT(type, assignment)  ${LUCENE_STATIC_CONSTANT_SYNTAX}
 
 /* Define to the necessary symbol if this constant uses a non-standard name on
    your system. */
+//todo: not checked
 #cmakedefine _CL_PTHREAD_CREATE_JOINABLE
 
-/* The size of a `unsigned char', as computed by sizeof. */
-#cmakedefine _CL_SIZEOF_UNSIGNED_CHAR
-
-/* The size of a `unsigned int', as computed by sizeof. */
-#cmakedefine _CL_SIZEOF_UNSIGNED_INT
-
-/* The size of a `unsigned long', as computed by sizeof. */
-#cmakedefine _CL_SIZEOF_UNSIGNED_LONG
-
-/* The size of a `unsigned long long', as computed by sizeof. */
-#cmakedefine _CL_SIZEOF_UNSIGNED_LONG_LONG
-
-/* The size of a `unsigned __int64', as computed by sizeof. */
-#cmakedefine _CL_SIZEOF_UNSIGNED___INT64
-
 /* Define to 1 if the `S_IS*' macros in <sys/stat.h> do not work properly. */
-#cmakedefine _CL_STAT_MACROS_BROKEN
+//todo: not being checked for...
+//#cmakedefine _CL_STAT_MACROS_BROKEN
 
 /* Define to 1 if you can safely include both <sys/time.h> and <time.h>. */
-#cmakedefine _CL_TIME_WITH_SYS_TIME  1 
+//not actually used for anything...
+//#cmakedefine _CL_TIME_WITH_SYS_TIME  1 
+
+/* Number of bits in a file offset, on hosts where this is settable. */
+//todo: should we only define for hosts where this is applicable?
+#define _FILE_OFFSET_BITS 64
+
+/* Define for large files, on AIX-style hosts. */
+//todo: should we only define for hosts where this is applicable?
+#define _LARGE_FILES
+
+/* Define that we will be using -fvisibility=hidden, and 
+ * make public classes visible using __attribute__ ((visibility("default")))
+ */
+#cmakedefine HAVE_GCCVISIBILITYPATCH 1
+
+/* Versions, etc */
 
 /* Version number of package */
-#cmakedefine _CL_VERSION  "0.9.15" 
+#define _CL_VERSION  "0.9.21-svn" 
+
+/* Name of package */
+#define _CL_PACKAGE  "clucene-core" 
+
+
+
+/* Configured options (from command line) */
 
 /* Forces into Ascii mode */
 #cmakedefine _ASCII
@@ -359,20 +263,8 @@ typedef float float_t;
 /* debuging option */
 #cmakedefine _DEBUG
 
-/* Number of bits in a file offset, on hosts where this is settable. */
-#cmakedefine _FILE_OFFSET_BITS
+/* Disable multithreading */
+#cmakedefine _CL_DISABLE_MULTITHREADING
 
-/* Define for large files, on AIX-style hosts. */
-#cmakedefine _LARGE_FILES
-
-/* If not already defined, then define as a datatype of *exactly* 32 bits. */
-#cmakedefine uint32_t
-
-/* If not already defined, then define as a datatype of *exactly* 64 bits. */
-#cmakedefine uint64_t
-
-/* If not already defined, then define as a datatype of *exactly* 8 bits. */
-#cmakedefine uint8_t
- 
 
 #endif
