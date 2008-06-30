8d9781995d94d297af37aa45fb5c2bb033fe6e03
ustramooner <ustramooner@20ef185c-fe11-0410-a618-ba9304b01011>
2008-06-30 16:58:59 +0000

    cmake improvements. conformity test script (experimental)
    git-svn-id: https://clucene.svn.sourceforge.net/svnroot/clucene/branches/lucene2_3_2@2700 20ef185c-fe11-0410-a618-ba9304b01011

---
+++ dist-test.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+#Check compliance with Coding standards...
+
+TMP=.
+
+#create environment...
+#cmake ..
+rm src/CLucene/_clucene-config.h 2>/dev/null
+
+#create header file for testing of symbols in headers.
+echo "#include \"CLucene/StdHeader.h"\" >$TMP/pub-headers.cpp
+
+#check to see that no #ifdefs exist in headers that don't belong
+function checkForIfdefs {
+    I=0
+    grep "#if" $1| while read line; do
+        I=`expr $I + 1`
+        if [ $I -gt 1 ]; then
+            echo $1 has invalid ifdef: $line
+            exit 1
+        fi
+    done
+}
+
+#iterate all headers
+for H in `find ../src/CLucene| grep "\.h$"`; do
+    BH=`basename $H`
+    if [ "${BH:0:1}" != "_" ]; then
+        DH=`dirname ${H:3}`
+        mkdir -p $DH 2>/dev/null
+    
+        cp $H ${H:3}
+        #checkForIfdefs $H
+        echo "#include \"${H:7}\"" >>$TMP/pub-headers.cpp
+    fi
+done
+
+#test if headers contain any symbols:
+echo "int main(){return 0;}"  >>$TMP/pub-headers.cpp
+#g++ -Isrc pub-headers.cpp
