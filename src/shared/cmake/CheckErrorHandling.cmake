85ba1492ed9daa9597990a035bc20c420d15ca1e
ustramooner <ustramooner@20ef185c-fe11-0410-a618-ba9304b01011>
2009-03-30 20:54:55 +0000

    check float<>byte conversions. check error handling properly
    git-svn-id: https://clucene.svn.sourceforge.net/svnroot/clucene/branches/lucene2_3_2@2972 20ef185c-fe11-0410-a618-ba9304b01011

--- src/shared/cmake/CheckErrorHandling.cmake
+++ src/shared/cmake/CheckErrorHandling.cmake
@@ -6,7 +6,7 @@ MACRO ( CHECK_HAVE_FUNCTION_TRY_BLOCKS result )
     CHECK_CXX_SOURCE_RUNS("
     	void foo() { try{ return; } catch( ... ){} }
     	int main(){ foo(); return 0; }" ${result})
-    IF ( NOT _CL_HAVE_TRY_BLOCKS )
-    	SET ( ${result} 1 )
-    ENDIF ( NOT _CL_HAVE_TRY_BLOCKS )
+    IF ( NOT ${result} )
+    	SET ( ${result} 1 FORCE)
+    ENDIF ( NOT ${result} )
 ENDMACRO ( CHECK_HAVE_FUNCTION_TRY_BLOCKS )
