499005d790a0c01feaf97ee044d042c69990b590
ustramooner <ustramooner@20ef185c-fe11-0410-a618-ba9304b01011>
2009-03-30 21:34:13 +0000

    various cleanups to the streams
    git-svn-id: https://clucene.svn.sourceforge.net/svnroot/clucene/branches/lucene2_3_2@2976 20ef185c-fe11-0410-a618-ba9304b01011

--- src/contribs-lib/CLucene/util/gzipcompressstream.h
+++ src/contribs-lib/CLucene/util/gzipcompressstream.h
@@ -17,29 +17,31 @@
  * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA 02110-1301, USA.
  */
-#ifndef GZIPCOMPRESSSTREAM_H
-#define GZIPCOMPRESSSTREAM_H
+#ifndef CLUENE_UTIL_GZIPCOMPRESSSTREAM_H
+#define CLUENE_UTIL_GZIPCOMPRESSSTREAM_H
 
-#include "bufferedstream.h"
+#include "CLucene/util/CLStreams.h"
 
 struct z_stream_s;
 
-namespace jstreams {
+CL_NS_DEF(util)
 
-class GZipCompressInputStream : public BufferedInputStream<char> {
+class GZipCompressInputStream : public InputStream{
 private:
-    z_stream_s* zstream;
-    StreamBase<char>* input;
-
-    void dealloc();
-    void readFromStream();
-    void compressFromStream();
+	 class Internal;
+	 Internal* internal;
 public:
-    explicit GZipCompressInputStream(StreamBase<char>* input, int level=-1);
-    ~GZipCompressInputStream();
-    int32_t fillBuffer(char* start, int32_t space);
+	 LUCENE_STATIC_CONSTANT(int32_t, DEFAULT_BUFFER_SIZE=4096);
+    explicit GZipCompressInputStream(InputStream* input, int level=-1);
+    virtual ~GZipCompressInputStream();
+	
+	 int32_t read(const signed char*& start, int32_t min, int32_t max);
+	 int64_t position();
+	 int64_t reset(int64_t);
+	 int64_t skip(int64_t ntoskip);
+	 size_t size();
+	 void setMinBufSize(int32_t minbufsize);
 };
 
-} // end namespace jstreams
-
+CL_NS_END
 #endif
