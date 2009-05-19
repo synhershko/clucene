0c17aaccd7c65145e484035bb5539351de8d299e
Ben <Ben@mini.(none)>
2009-05-19 08:29:39 +0200

    revert reuters change

--- src/test/index/TestReuters.cpp
+++ src/test/index/TestReuters.cpp
@@ -74,7 +74,6 @@ CL_NS_USE(util)
 		char* tmppathP = tmppath + strlen(tmppath);
 		TCHAR tpath[CL_MAX_PATH];
 
-    int c = 0;
 		while ( fl != NULL ){
 			strcpy(tmppathP,fl->d_name);
 			STRCPY_AtoT(tpath,fl->d_name,CL_MAX_PATH);
@@ -87,8 +86,6 @@ CL_NS_USE(util)
 				writer.addDocument( doc );
 				_CLDELETE(doc);
 
-        if ( c++ % 2 == 0 ) 
-          writer.flush();
 			}
 			fl = readdir(srcdir);
 		}
