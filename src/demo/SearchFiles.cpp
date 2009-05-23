47b77b4aec0b26990cb034c8b806c94a3519e284
Ben van KLinken <ustramooner@users.sourceforge.net>
2009-05-23 18:50:51 +0200

    fixed demo compile

--- src/demo/SearchFiles.cpp
+++ src/demo/SearchFiles.cpp
@@ -1,7 +1,7 @@
 /*------------------------------------------------------------------------------
 * Copyright (C) 2003-2006 Ben van Klinken and the CLucene Team
-* 
-* Distributable under the terms of either the Apache License (Version 2.0) or 
+*
+* Distributable under the terms of either the Apache License (Version 2.0) or
 * the GNU Lesser General Public License, as specified in the COPYING file.
 ------------------------------------------------------------------------------*/
 #include "stdafx.h"
@@ -26,7 +26,7 @@ void SearchFiles(const char* index){
 	standard::StandardAnalyzer analyzer;
 	char line[80];
 	TCHAR tline[80];
-	const TCHAR* buf;
+	TCHAR* buf;
 
   IndexReader* reader = IndexReader::open(index);
   while (true) {
@@ -75,5 +75,5 @@ break;
 	}
   _CLDELETE(reader);
 	//delete line;
-}	
+}
 
